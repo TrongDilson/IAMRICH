bff18ee77354f3d402a4dfce964209b5406aadf9
TranTrong <s3863973@rmit.edu.vn>
2024-07-17 11:16:12 +0700

    Update Hello World

--- IAMRICH/ContentView.swift
+++ IAMRICH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world HELLO!")
+            Text("Hello, world HELLO 2 U!")
         }
         .padding()
     }
