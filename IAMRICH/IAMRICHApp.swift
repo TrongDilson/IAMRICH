24a56699161e477eb793a840ed8031fd0e290c81
TranTrong <s3863973@rmit.edu.vn>
2024-07-17 11:09:20 +0700

    Initial Commit

---
+++ IAMRICH/IAMRICHApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAMRICHApp.swift
+//  IAMRICH
+//
+//  Created by Trong Tran Vinh on 17/7/24.
+//
+
+import SwiftUI
+
+@main
+struct IAMRICHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
